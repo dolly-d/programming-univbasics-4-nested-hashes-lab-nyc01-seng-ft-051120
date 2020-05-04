49e3d929171f14ca094cdd7efa756f275ced9da1
Dolly Desir <dolly.desir@gmail.com>
2020-05-04 04:24:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,9 +133,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie] = [
-  {:languages => 'C'},
-  {:languages => 'Assembly'}
-  ]
+programmer_hash[:dennis_ritchie][:languages] + 'Assembly'
+
   return programmer_hash
 end
