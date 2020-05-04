bc64bba8ad106761effd0e173686a997dc5c2555
Dolly Desir <dolly.desir@gmail.com>
2020-05-04 04:27:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -13,8 +13,10 @@ def hopper
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
+    
       }
-    }
+ 		}   
+programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -68,6 +70,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+ 		   :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+ 		  },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +87,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  
 end
 
 def changing_alan
@@ -103,6 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for] = 'GUI'
+  return programmer_hash
 
 end
 
@@ -125,5 +133,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+programmer_hash[:dennis_ritchie][:languages].push 'Assembly'
 
+  return programmer_hash
 end
